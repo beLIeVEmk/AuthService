381951ba178ebaf723b3c9b6c771301fa37f34b8
karthikeyan <karthikeyan.muthukrishnan@havells.com>
2024-09-23 19:24:38 +0530

    Authorization-roles

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const serverConfig = require("./config/serverConfig");
 const apiRoutes=require('./routes/index')
 const app=express()
-
+const db=require('./models/index')
 
 
 const startServer=()=>{
     app.use(express.json())
     app.use('/api',apiRoutes)
     app.listen(serverConfig.PORT,()=>{
+        //db.sequelize.sync({alert:true})
         console.log(`Server running at port ${serverConfig.PORT}`)
     })
 }
--- src/models/user.js
+++ src/models/user.js
@@ -12,6 +12,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      User.belongsToMany(models.Role,{
+        through:'User_Roles'
+      })
       // define association here
     }
   }
